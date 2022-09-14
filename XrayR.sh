0a4812a19e2a1278c454906e8e6df293967858ea
maissbacku <100511293+maissbacku@users.noreply.github.com>
2022-09-14 13:31:27 +0700

    Update XrayR.sh

--- XrayR.sh
+++ XrayR.sh
@@ -465,11 +465,13 @@ Nodes:
           CLOUDFLARE_EMAIL: phanhaine@gmail.com
           CLOUDFLARE_API_KEY: 5d0fdee9b8a3d734928f25b92148a766d5617
 EOF
-        elif  [[ $NodeType == 2 ]]; then
-            read -rp "Nhập ID Node ID: " NodeID
+        fi  
+        read -rp "Nhập ID Node ID: " NodeID
+        if [[ $NodeType == "2" ]]; then
             read -rp "Vui lòng nhập domain Trojan [443]：" DomainPort
-            read -rp "Tốc độ giới hạn trên server:" SpeLimit
-            read -rp "Giới hạn số người dùng:" UserLimit
+        fi
+        read -rp "Tốc độ giới hạn trên server:" SpeLimit
+        read -rp "Giới hạn số người dùng:" UserLimit
         cd /etc/XrayR
         mv config.yml config.yml.bak
         cat <<EOF > /etc/XrayR/config.yml
@@ -494,7 +496,7 @@ Nodes:
       ApiHost: "https://datathaga.com"
       ApiKey: "4fCdmbVBjnVUByVC"
       NodeID: $NodeID
-      NodeType: Trojan # Node type: V2ray, Shadowsocks, Trojan, Shadowsocks-Plugin
+      NodeType: $NodeType # Node type: V2ray, Shadowsocks, Trojan, Shadowsocks-Plugin
       Timeout: 30 # Timeout for the api request
       EnableVless: false # Enable Vless for V2ray Type
       EnableXTLS: false # Enable XTLS for V2ray and Trojan
@@ -530,71 +532,6 @@ Nodes:
           CLOUDFLARE_EMAIL: phanhaine@gmail.com
           CLOUDFLARE_API_KEY: 5d0fdee9b8a3d734928f25b92148a766d5617
 EOF
-        else
-            read -rp "Nhập ID Node VMESS: " NodeID
-            read -rp "Tốc độ giới hạn trên server:" SpeLimit
-            read -rp "Giới hạn số người dùng:" UserLimit
-        cd /etc/XrayR
-        mv config.yml config.yml.bak
-        cat <<EOF > /etc/XrayR/config.yml
-Log:
-  Level: none # Log level: none, error, warning, info, debug 
-  AccessPath: # /etc/XrayR/access.Log
-  ErrorPath: # /etc/XrayR/error.log
-DnsConfigPath: # /etc/XrayR/dns.json # Path to dns config, check https://xtls.github.io/config/base/dns/ for help
-InboundConfigPath: # /etc/XrayR/custom_inbound.json # Path to custom inbound config, check https://xtls.github.io/config/inbound.html for help
-RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/base/route/ for help
-OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/base/outbound/ for help
-ConnetionConfig:
-  Handshake: 4 # Handshake time limit, Second
-  ConnIdle: 30 # Connection idle time limit, Second
-  UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
-  DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
-  BufferSize: 64 # The internal cache size of each connection, kB 
-Nodes:
-  -
-    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
-    ApiConfig:
-      ApiHost: "https://datathaga.com"
-      ApiKey: "4fCdmbVBjnVUByVC"
-      NodeID: $NodeID
-      NodeType: Vmess # Node type: V2ray, Shadowsocks, Trojan, Shadowsocks-Plugin
-      Timeout: 30 # Timeout for the api request
-      EnableVless: false # Enable Vless for V2ray Type
-      EnableXTLS: false # Enable XTLS for V2ray and Trojan
-      SpeedLimit: $SpeLimit # Mbps, Local settings will replace remote settings, 0 means disable
-      DeviceLimit: $UserLimit # Local settings will replace remote settings, 0 means disable
-      RuleListPath: # /etc/XrayR/rulelist Path to local rulelist file
-    ControllerConfig:
-      ListenIP: 0.0.0.0 # IP address you want to listen
-      SendIP: 0.0.0.0 # IP address you want to send pacakage
-      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
-      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
-      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
-      DisableUploadTraffic: false # Disable Upload Traffic to the panel
-      DisableGetRule: false # Disable Get Rule from the panel
-      DisableIVCheck: false # Disable the anti-reply protection for Shadowsocks
-      DisableSniffing: true # Disable domain sniffing 
-      EnableProxyProtocol: false # Only works for WebSocket and TCP
-      EnableFallback: false # Only support for Trojan and Vless
-      FallBackConfigs:  # Support multiple fallbacks
-        -
-          SNI: # TLS SNI(Server Name Indication), Empty for any
-          Path: # HTTP PATH, Empty for any
-          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
-          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
-      CertConfig:
-        CertMode: dns # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
-        CertDomain: "$DomainPort" # Domain to cert
-        CertFile: /etc/XrayR/cert/$DomainPort.cert # Provided if the CertMode is file
-        KeyFile: /etc/XrayR/cert/$DomainPort.key
-        Provider: cloudflare # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
-        Email: phanhaine@gmail.com
-        DNSEnv: # DNS ENV option used by DNS provider
-          CLOUDFLARE_EMAIL: phanhaine@gmail.com
-          CLOUDFLARE_API_KEY: 5d0fdee9b8a3d734928f25b92148a766d5617
-EOF   
-        fi
         echo -e "${green}XrayR The configuration file is generated，restarting XrayR service ${plain}"
         restart 0
         before_show_menu
